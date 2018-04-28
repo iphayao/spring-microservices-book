fc03c57b12781a1aa16f1c632218ad984c5eec8e
iphayao <phayaob@gmail.com>
2018-04-28 20:06:21 +0700

    Spring Boot microservice suing Spring Initializer (HATEOAS)

---
+++ Chapter2/chapter2.boothateoeas/src/main/java/com/phayao/chapter2/Application.java
@@ -0,0 +1,64 @@
+package com.phayao.chapter2;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
+
+@RestController
+class GreetingController {
+	
+	@RequestMapping("/greet")
+	Greet greet() {
+		return new Greet("Hello World!");
+	}
+	
+	@RequestMapping("/greeting")
+	@ResponseBody
+	public HttpEntity<Greet> greeting(@RequestParam(value = "name", required = false, defaultValue = "HATEOAS") String name) {
+		Greet greet = new Greet("Hello " + name);
+		greet.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+		
+		return new ResponseEntity<Greet>(greet, HttpStatus.OK);
+	}
+	
+}
+
+class Greet extends ResourceSupport {
+	private String message;
+	
+	public Greet() {}
+	
+	public Greet(String message) {
+		this.setMessage(message);		
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
+
+
