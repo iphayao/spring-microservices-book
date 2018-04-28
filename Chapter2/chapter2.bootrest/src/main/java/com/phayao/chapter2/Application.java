71cc87ee735553554cab4ad151222834aefb1bb4
iphayao <phayaob@gmail.com>
2018-04-28 20:55:12 +0700

    Securing microservice with basic security

--- Chapter2/chapter2.bootrest/src/main/java/com/phayao/chapter2/Application.java
+++ Chapter2/chapter2.bootrest/src/main/java/com/phayao/chapter2/Application.java
@@ -2,9 +2,11 @@ package com.phayao.chapter2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableGlobalMethodSecurity
 @SpringBootApplication
 public class Application {
 
--- Chapter2/chapter2.bootrest/src/test/java/com/phayao/chapter2/ApplicationTests.java
+++ Chapter2/chapter2.bootrest/src/test/java/com/phayao/chapter2/ApplicationTests.java
@@ -5,6 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.*;;
@@ -16,10 +21,20 @@ public class ApplicationTests {
 	@Autowired
 	private TestRestTemplate restTemplate;
 	
-	@Test
+	/*@Test
 	public void testVanillaService() {
 		Greet greet = restTemplate.getForObject("/", Greet.class);
 		Assert.assertEquals("Hello World!", greet.getMessage());
+	}*/
+	
+	@Test
+	public void testSecureService() {
+		String plainCreds = "guest:guest1234";
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Basic " + new String(Base64.encode(plainCreds.getBytes())));
+		HttpEntity<String> request = new HttpEntity<String>(headers);
+		ResponseEntity<Greet> response = restTemplate.exchange("/", HttpMethod.GET, request, Greet.class);
+		Assert.assertEquals("Hello World!", response.getBody().getMessage());
 	}
 
 }
